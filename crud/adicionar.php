3d2959a57e6112f03091455eea305daa3f1adb03
renatogual <renatogual@hotmail.com>
2021-02-17 14:16:50 -0300

    Criando estrutura da pagina de adicionar os clientes

---
+++ crud/adicionar.php
@@ -0,0 +1,37 @@
+<?php
+include_once 'includes/header.php'
+?>
+
+<div class="row">
+  <div class="col s12 m6 push-m3">
+    <h3 class="light">Novo Cliente</h3>
+    <form action="" method="post">
+      <div class="input-field col s12">
+        <input type="text" name="nome" id="nome">
+        <label for="nome">Nome</label>
+      </div>
+
+      <div class="input-field col s12">
+        <input type="text" name="sobrenome" id="sobrenome">
+        <label for="sobrenome">Sobrenome</label>
+      </div>
+
+      <div class="input-field col s12">
+        <input type="text" name="email" id="email">
+        <label for="email">Email</label>
+      </div>
+
+      <div class="input-field col s12">
+        <input type="text" name="idade" id="idade">
+        <label for="idade">Idade</label>
+      </div>
+
+      <button type="submit" class="btn">Cadastrar</button>
+      <button type="submit" class="btn green">Lista de clientes</button>
+    </form>
+  </div>
+</div>
+
+<?php
+include_once 'includes/footer.php'
+?>
